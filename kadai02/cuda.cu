e399e844d7a851013fc80dded7715790453cd05e
adelie-penguin <nakamura@mae.cs.it-chiba.ac.jp>
2024-10-22 16:21:19 +0900

    Create cuda.cu

---
+++ kadai02/cuda.cu
@@ -0,0 +1,41 @@
+/* vim: set tabstop=4 : */
+/****************************************************************************
+ * cuda のおためしプログラム
+ *----------------------------------------------------------------------
+ *
+ * コンパイルコマンド
+ * $make
+ * 実行コマンド
+ * $./a.out 
+ *
+ ****************************************************************************/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+/* CUDAが無効な状態でも実行したい場合はこのifdefに代替コードを記述 */
+#ifdef __CUDACC__
+#include<cuda_runtime.h>
+#define penguin_print()	fprintf(stdout, ">> with cuda\n")
+#else
+#define penguin_print()	fprintf(stdout, ">> without cuda\n")
+#endif
+/*------------*/
+
+#define BLOCK_MAX 	2	/* スレッドブロック数 */
+#define BLOCK_SIZE  4	/* スレッド数 */
+
+void* function(void*);
+
+int main(int argc, char *argv[])
+{
+	penguin_print();
+	function<<<BLOCK_MAX, BLOCK_SIZE>>>("hello cuda!");
+	cudaDeviceReset();
+	return(0);
+}
+
+__global__ void function(char *m)
+{
+	printf(">> block%d thread%d %s", blockIdx.x, threadIdx.x, m);
+}
