f11ecf92d43e6581a6b9aeb7960aaca28f513431
adelie-penguin <nakamura@mae.cs.it-chiba.ac.jp>
2024-10-26 23:58:14 +0900

    Update python01.py

--- kadai02/python01.py
+++ kadai02/python01.py
@@ -1,6 +1,6 @@
 #********************************************************
 #
-# Python 並列処理(Pool)の実験プログラム
+# Python Poolの実験プログラム
 #
 #---------------------------------------------------------
 #
@@ -9,22 +9,31 @@
 #
 #********************************************************
 
-
+import numpy as np
+import os
 from multiprocessing import Pool
+
 
 #{{{
 def function(arg_value):
-    p_max, num = arg_value
-    print(">> process", num, "/", p_max)
+
+    pool_num, num = arg_value
+    print(">> PID:", os.getpid(), " arg_value:", num, "/", pool_num)
+
+    return num
 #}}}
 
 
 if(__name__ == '__main__'):
 
     loop_num = 10
-    p = Pool(loop_num)
-    arg_value = [(loop_num , i) for i in range(loop_num)]
-    tmp = p.map(function, arg_value)
-    
+    pool_num = 4
+
+    p = Pool(pool_num)
+    arg_value = [(pool_num , i) for i in range(loop_num)]
+    result = p.map(function, arg_value)
+    print(">> result:", result)
+
     #p.terminate()
     #p.join()
+
