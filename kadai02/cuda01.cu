98cacb29fe1d99f9fce6260406b06b2507ecf576
adelie-penguin <nakamura@mae.cs.it-chiba.ac.jp>
2024-10-27 00:03:00 +0900

    Update and rename cuda2.cu to cuda01.cu

--- kadai02/cuda01.cu
+++ kadai02/cuda01.cu
@@ -6,21 +6,12 @@
  * コンパイルコマンド
  * $make
  * 実行コマンド
- * $./cuda2 
+ * $./cuda01 
  *
  ****************************************************************************/
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
-/* CUDAが無効な状態でも実行したい場合はこのifdefに代替コードを記述 */
-#ifdef __CUDACC__
-#include<cuda_runtime.h>
-#define penguin_print()	fprintf(stdout, ">> with cuda\n")
-#else
-#define penguin_print()	fprintf(stdout, ">> without cuda\n")
-#endif
-/*------------*/
 
 #define BLOCK_MAX   2	/* スレッドブロック数 */
 #define BLOCK_SIZE  4	/* スレッド数 */
@@ -29,7 +20,6 @@ __global__ void function(void);
 
 int main(int argc, char *argv[])
 {
-	penguin_print();
 	function<<<BLOCK_MAX, BLOCK_SIZE>>>();
 	cudaDeviceReset();
 	return(0);
